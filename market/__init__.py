bb57a91a1ba2dd600227712f85d1995fbe7b7a00
Juan Chacon <mahnimex@gmail.com>
2021-07-26 15:38:33 -0400

    feat: forms

--- market/__init__.py
+++ market/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///market.db"
+app.config["SECRET_KEY"] = "6d7e4ad62ff9528d84a4625d"
 db = SQLAlchemy(app)
 
 from market import routes
--- market/routes.py
+++ market/routes.py
@@ -1,6 +1,7 @@
-from market import app
-from flask import render_template
-from market.models import Item
+from market import app, db
+from flask import render_template, redirect, url_for, flash
+from market.models import Item, User
+from market.forms import RegisterForm
 
 
 @app.route("/")
@@ -13,3 +14,25 @@ def home_page():
 def market_page():
     items = Item.query.all()
     return render_template("market.html", items=items)
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register_page():
+    form = RegisterForm()
+
+    if form.validate_on_submit():
+        user_to_create = User(
+            userName=form.username.data,
+            email_address=form.email_address.data,
+            password_hash=form.password1.data,
+        )
+
+        db.session.add(user_to_create)
+        db.session.commit()
+        return redirect(url_for("market_page"))
+
+    if form.errors is not {}:  # If there are not errors from the validations
+        for err_msg in form.errors.values():
+            flash(f"There was an error creating an user: {err_msg}", category="danger")
+
+    return render_template("register.html", form=form)
