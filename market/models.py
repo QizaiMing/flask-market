da8f93ea7405fa5bdc61a127d6b87aa24d08577e
Juan Chacon <juan@stracos.com>
2021-07-31 15:29:08 -0400

    feat: buying/selling

--- market/models.py
+++ market/models.py
@@ -35,6 +35,12 @@ class User(db.Model, UserMixin):
     def check_password_correction(self, attempted_password):
         return bcrypt.check_password_hash(self.password_hash, attempted_password)
 
+    def can_purchase(self, item_obj):
+        return self.budget >= item_obj.price
+
+    def can_sell(self, item_obj):
+        return self.id == item_obj.owner
+
 
 class Item(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -46,3 +52,13 @@ class Item(db.Model):
 
     def __repr__(self):
         return f"Item {self.name}"
+
+    def buy(self, user):
+        self.owner = user.id
+        user.budget -= self.price
+        db.session.commit()
+
+    def sell(self, user):
+        self.owner = None
+        user.budget += self.price
+        db.session.commit()
